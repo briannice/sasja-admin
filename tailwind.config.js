011caabe62b96dd3d6203ab032bad7f30b8a016a
briannice <brian.nys.dev@gmail.com>
2022-08-16 18:19:19 +0200

    tailwind color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,50 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.tsx",
-    "./src/components/**/*.tsx",
-    "./src/layouts/**/*.tsx",
-    "./src/icons/**/*.tsx",
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+    './src/icons/**/*.tsx',
   ],
   theme: {
+    colors: {
+      transparent: colors.transparent,
+
+      white: colors.white,
+      light: colors.gray[100],
+      medium: colors.gray[300],
+      gray: colors.gray[500],
+      dark: colors.gray[700],
+      black: colors.gray[900],
+
+      primary: {
+        DEFAULT: '#EB2C2C',
+        dark: '#CC1313',
+      },
+
+      info: {
+        DEFAULT: colors.blue[500],
+        dark: colors.blue[600],
+      },
+
+      success: {
+        DEFAULT: colors.green[500],
+        dark: colors.green[600],
+      },
+
+      error: {
+        DEFAULT: colors.red[500],
+        dark: colors.red[600],
+      },
+
+      warning: {
+        DEFAULT: colors.orange[500],
+        dark: colors.orange[600],
+      },
+    },
     extend: {},
   },
   plugins: [],
-};
+}
