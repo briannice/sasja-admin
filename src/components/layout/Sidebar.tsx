7cbba2e0f6c69fbe7151073e2d397e76a59a4212
briannice <brian.nys.dev@gmail.com>
2022-08-17 17:53:47 +0200

    anchor focus state

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -40,7 +40,7 @@ export default function Sidebar() {
                 <Link
                   href={link.url}
                   className={clsx(
-                    'flex items-center space-x-4 rounded-lg border px-4 py-2',
+                    'flex items-center space-x-4 rounded-lg border-2 px-4 py-2',
                     isSelected
                       ? 'border-primary bg-primary'
                       : 'border-white group-hover:border-primary'
