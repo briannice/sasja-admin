b3f5a46a9089628fe4a9b45358330caacaca236a
briannice <brian.nys.dev@gmail.com>
2022-08-25 21:10:41 +0200

    home page cleanup

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,38 +1,9 @@
-import TextEditor from '@/components/editor/TextEditor'
-import DateInput from '@/components/form/DateInput'
-import DateTimeInput from '@/components/form/DateTimeInput'
-import SelectInput from '@/components/form/SelectInput'
-import TextInput from '@/components/form/TextInput'
-import { Timestamp } from 'firebase/firestore'
-import React, { useState } from 'react'
-
-const vs = [
-  { key: 'a', value: 'Haha' },
-  { key: 'b', value: 'Hehe' },
-  { key: 'c', value: 'Hoho' },
-  { key: 'd', value: 'Hihi' },
-  { key: 'e', value: 'Huhu' },
-  { key: 'f', value: 'Hyhy' },
-]
+import React from 'react'
 
 export default function DashboardPage() {
-  const [text, setText] = useState('')
-  const [editor, setEditor] = useState('')
-  const [timestamp, setTimestamp] = useState(Timestamp.now())
-  const [v, setV] = useState(vs[0].key)
-
   return (
     <>
       <h1>Dashboard</h1>
-      <div className="mt-8 grid grid-cols-2 gap-8">
-        <TextInput type="text" name="Naam" value={text} onChange={setText} />
-        <TextInput type="email" name="Naam" value={text} onChange={setText} />
-        <TextInput type="password" name="Naam" value={text} onChange={setText} />
-        <DateInput name="Datum" value={timestamp} onChange={setTimestamp} />
-        <DateTimeInput name="Datum" value={timestamp} onChange={setTimestamp} />
-        <SelectInput name="SelectInput" values={vs} value={v} onChange={setV} />
-        <TextEditor value={editor} onChange={setEditor} className="col-span-2" />
-      </div>
     </>
   )
 }
