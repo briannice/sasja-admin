82b4e97fabd4c23b06eaf31d70a36bc83ec1d544
kclaes <kenny.claes@gmail.com>
2024-09-08 16:22:38 +0200

    fix: patch in dutch teams

--- src/services/hb/clubs.ts
+++ src/services/hb/clubs.ts
@@ -14,6 +14,41 @@ export const getHandballBelgiumClubs = async () => {
     short: e.short_name,
     logo: e.logo_img_url || null,
   }))
+    // patch 6 SHL Dutch teams in
+    .concat(dutchClubs)
 
   return clubs
 }
+
+const dutchClubs =
+  [{
+    id: 8881,
+    name: 'Green Park Aalsmeer',
+    short: 'Aalsmeer',
+    logo: '/clubs/aalsmeer.png',
+  }, {
+    id: 8882,
+    name: 'Herpertz Bevo',
+    short: 'Bevo',
+    logo: '/clubs/bevo.png',
+  }, {
+    id: 8883,
+    name: 'Handbal Houten',
+    short: 'Houten',
+    logo: '/clubs/houten.png',
+  }, {
+    id: 8884,
+    name: 'Hurry-up',
+    short: 'Hurry-up',
+    logo: '/clubs/hurry-up.png',
+  }, {
+    id: 8885,
+    name: 'Kembit Lions Sittardia',
+    short: 'Lions',
+    logo: '/clubs/lions.png',
+  }, {
+    id: 8886,
+    name: 'Kras Volendam',
+    short: 'Volendam',
+    logo: '/clubs/volendam.png',
+  }]
