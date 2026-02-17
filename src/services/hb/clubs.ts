1182d31899de2ecbcba8b40f7628c73ad7e56df2
kclaes <kenny.claes@gmail.com>
2026-02-17 17:12:40 +0100

    update nextjs16

--- src/services/hb/clubs.ts
+++ src/services/hb/clubs.ts
@@ -1,12 +1,11 @@
-import { HandballBelgiumApi } from '@/services/hb'
 import { OpponentModel } from '@/types/models'
 
 export const getHandballBelgiumClubs = async () => {
-  const { data, status } = await HandballBelgiumApi.get(
-    'club/byMyLeague?season_id=2&sort[0]=short_name&sort[1]=reference&sort[2]=order&club_status_id=1'
-  )
+  const response = await fetch('/api/hb/clubs')
 
-  if (status !== 200) return []
+  if (!response.ok) return []
+
+  const data = await response.json()
 
   const clubs: OpponentModel[] = data.elements.map((e: any) => ({
     id: e.reference,
